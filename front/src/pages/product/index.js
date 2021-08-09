9efff8df08ff9330215104f2afb850fbfd0507f9
uuuuujin <yearn8546@gmail.com>
2021-08-09 17:39:32 +0900

    두근

--- front/src/pages/product/index.js
+++ front/src/pages/product/index.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import Layout from '../../components/Layout'
+import React from 'react';
+import styled from 'styled-components'
+import Layout from '../../components/Layout';
+import Top from './components/top';
+import Bottom from './components/bottom';
+
+const MainWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const SubWrap = styled.div`
+  width: 1024px;
+`
+
 
 const Product = () => {
   return (
     <Layout>
-      
+      <MainWrap>
+        <SubWrap>
+          <Top/>
+          <Bottom/>
+        </SubWrap>
+      </MainWrap>
     </Layout>
   )
 }
